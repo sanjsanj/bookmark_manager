13764f45feac5be043b54a8917b3f60ead053752
sanjsanj <sanjsanj@hotmail.com>
2015-04-06 19:58:33 +0100

    Can add tags to links

--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -3,9 +3,11 @@ require 'data_mapper'
 
 env = ENV['RACK_ENV'] || 'development'
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-require './lib/link'
+require 'link'
+require 'tag'
 DataMapper.finalize
 DataMapper.auto_upgrade!
+
 
 class BookmarkManager < Sinatra::Base
   get '/' do
@@ -14,9 +16,13 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
+
     url = params["url"]
     title = params["title"]
-    Link.create(:url => url, :title => title)
+    tags = params["tags"].split(" ").map do |tag|
+      Tag.first_or_create(:text => tag)
+    end
+    Link.create(:url => url, :title => title, :tags => tags)
     redirect to '/'
   end
 
